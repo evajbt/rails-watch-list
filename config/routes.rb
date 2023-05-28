ea7be2bac8ede5dea89debfdbcad967c33412431
Eva Joubert <evajoubert@live.fr>
2023-05-28 12:13:51 +0200

    Watch List without front

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'lists', to: 'lists#index'
+  get 'lists/new', to: 'lists#new', as: 'new_list'
+  post 'lists', to: 'lists#create'
+  get 'lists/:id', to: 'lists#show', as: 'list'
+  get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: 'new_bookmark'
+  post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: 'create_bookmark'
+  delete 'bookmarks/:id', to: 'bookmarks#destroy', as: 'bookmark'
 end
